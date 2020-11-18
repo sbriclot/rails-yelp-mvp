40f3a04933464b74dda776cd30d91d7f3e9b4167
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-18 15:40:21 +0100

    Yelp MVP before embedding reviews in show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Create 10 restaurants..."
+
+10.times do 
+  name = Faker::Restaurant.name
+  address = Faker::Address.city
+  phone = Faker::PhoneNumber.phone_number_with_country_code
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create!(name: name, address: address, phone_number: phone, category: category)
+end
+
+puts "Done"
